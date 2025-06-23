9083c1be431436f0a92ff727ffaa95d52d69c1c7
Balkishan Bajpay <bkbajpay0905@gmail.com>
2025-06-23 21:53:54 +0530

    Update subset sum equal to target.cpp

--- recursion/subset sum equal to target.cpp
+++ recursion/subset sum equal to target.cpp
@@ -31,3 +31,34 @@ class Solution {
         return count;
     }
 };
+
+T.C : O(n*2^n)
+
+// updated version
+T.C : O(2^n)
+class Solution {
+  public:
+    void subseq(int index, int size, int target, int sum, int &count, vector<int> arr)
+    {
+        if(index == size)
+        {
+            if(sum == target)
+            {
+                count++;
+            }
+            return;
+        }
+        // nhi lena
+        subseq(index+1, size, target, sum, count, arr);
+        // lena h
+        subseq(index+1, size, target, sum + arr[index], count, arr);
+    }
+    int perfectSum(vector<int>& arr, int target) {
+        // code here
+        vector<int> temp;
+        int sum = 0;
+        int count = 0;
+        subseq(0, arr.size(), target, sum, count, arr);
+        return count;
+    }
+};
