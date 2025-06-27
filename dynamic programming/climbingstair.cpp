8aaa56799de57cf6171331ab863eb2f429adc275
Balkishan Bajpay <bkbajpay0905@gmail.com>
2025-06-27 13:55:16 +0530

    Update climbingstair.cpp

--- dynamic programming/climbingstair.cpp
+++ dynamic programming/climbingstair.cpp
@@ -37,3 +37,62 @@ public:
         return sum;
     }
 };
+
+
+
+//  memoization
+
+class Solution {
+public:
+    int find (int index, vector<int>& nums, int size, vector<int> &dp)
+    {
+        if(index >= size)
+        {
+            return 0;
+        }
+        if(dp[index] != -1)
+        {
+            return dp[index];
+        }
+        return dp[index] = max(nums[index] + find(index+2, nums, size, dp), find(index+1, nums, size, dp));
+    }
+    int rob(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> dp(size+1, -1);
+        find(0, nums, size, dp);
+        return dp[0];
+    }
+};
+
+// tabulation
+
+int rob(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> dp(size+2, -1);
+        // find(0, nums, size, dp);
+        dp[size] = 0;
+        dp[size+1] = 0;
+        for(int i = size-1; i >= 0; i--)
+        {
+            dp[i] = max(nums[i] + dp[i+2], dp[i+1]);
+        }
+        return dp[0];
+    }
+
+// space optimization
+
+ int rob(vector<int>& nums) {
+        int size = nums.size();
+        // vector<int> dp(size+2, -1);
+        // find(0, nums, size, dp);
+        // dp[size] = 0;
+        // dp[size+1] = 0;
+        int last = 0, secondLast = 0;
+        int sum = 0;
+        for(int i = size-1; i >= 0; i--)
+        {
+            sum = max(nums[i] + last, secondLast);
+            last = secondLast;
+            secondLast = sum;
+        }
+        return sum;
