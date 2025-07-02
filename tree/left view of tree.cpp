122b0c35164995d4d28b6db9b623ed90dcdbe80a
Balkishan Bajpay <bkbajpay0905@gmail.com>
2025-07-02 16:16:46 +0530

    Update left view of tree.cpp

--- tree/left view of tree.cpp
+++ tree/left view of tree.cpp
@@ -45,3 +45,53 @@ class Solution {
         return ans;
     }
 };
+
+// using recursion
+
+/* A binary tree node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+class Solution {
+  public:
+    int height(Node* root)
+    {
+        if(root == NULL)
+            return NULL;
+        return 1 + max(height(root->left), height(root->right));
+        
+    }
+    void find(Node* root, vector<int> &ans, vector<bool> &visited, int level)
+    {
+        if(root == NULL)
+            return;
+        
+        if(visited[level] == 0)
+        {
+            ans.push_back(root->data);
+            visited[level] = 1; 
+        }
+        find(root->left, ans, visited, level+1);
+        find(root->right, ans, visited, level+1);
+    }
+    vector<int> leftView(Node *root) {
+        // code here
+       int levels = height(root);
+       vector<bool>visited(levels,0);
+       vector<int> ans;
+       int level = 0;
+       find(root, ans, visited, level);
+       return ans;
+    }
+};
