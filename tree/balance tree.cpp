c3422a4b903fe4be010523542e3cae0e1cc6944b
Balkishan Bajpay <bkbajpay0905@gmail.com>
2025-07-03 09:37:27 +0530

    Create balance tree.cpp

---
+++ tree/balance tree.cpp
@@ -0,0 +1,39 @@
+/* A binary tree node structure
+
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+ */
+
+class Solution {
+  public:
+    int height(Node* root, bool &valid)
+    {
+        if(root==NULL)
+            return 0;
+        int l = height(root->left, valid);
+        int r = height(root->right, valid);
+        
+        if(abs(l-r) > 1)
+        {
+            valid = 0;
+        }
+        return 1 + max(l, r);
+    }
+    bool isBalanced(Node* root) {
+        // Code here
+        bool valid = 1;
+        height(root, valid);
+        return valid;
+    }
+};
