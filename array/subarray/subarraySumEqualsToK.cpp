1c41b6686a723c94d5bab07aff55f6f434b83c75
Balkishan <bkbajpay0609@gmail.com>
2025-08-18 09:39:35 +0530

    subarray

---
+++ array/subarray/subarraySumEqualsToK.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        int sum = 0;
+        mp[0]++;
+        int count = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum += nums[i];
+            int rem = sum - k;
+            if(mp.find(rem) != mp.end())
+            {
+                count += mp[rem];
+            } 
+            mp[sum]++;
+        }
+        return count;
+    }
+};
